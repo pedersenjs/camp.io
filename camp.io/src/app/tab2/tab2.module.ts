996c946d8e7ec4d82699e5d81ba2613636f93cf1
Jenna Pedersen <jspeders@ncsu.edu>
2022-09-13 15:23:29 -0400

    Adding add location page

--- camp.io/src/app/tab2/tab2.module.ts
+++ camp.io/src/app/tab2/tab2.module.ts
@@ -1,10 +1,10 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+// import { GoogleMap } from '@capacitor/google-maps';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
 
@@ -18,4 +18,6 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
   ],
   declarations: [Tab2Page]
 })
+
+
 export class Tab2PageModule {}
