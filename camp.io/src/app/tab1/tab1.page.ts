dc58b637ccce9e19dce4d2b4090d8defb0b49e3a
turnerpat <turnerpatrick21@gmail.com>
2022-09-22 22:04:01 -0400

    Openstreetmap working on tab1

--- camp.io/src/app/tab1/tab1.page.ts
+++ camp.io/src/app/tab1/tab1.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import * as L from "leaflet"
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
+  map: L.Map
 
   constructor() {}
 
+  ngOnInit() {
+    this.map = L.map('map', {
+      center: [ 25.3791924,55.4765436 ],
+      zoom: 15,
+      renderer: L.canvas()
+    })
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap',
+    }).addTo(this.map)
+
+
+    setTimeout(() => {
+      this.map.invalidateSize();
+    }, 0);
+  }
 }
