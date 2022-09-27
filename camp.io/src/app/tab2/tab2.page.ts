b3f52fd0b1fe540a41a322546dc387ef08f74c11
Jenna Pedersen <jspeders@ncsu.edu>
2022-09-27 16:29:09 -0400

    Updated to include an x and y coordinate form

--- camp.io/src/app/tab2/tab2.page.ts
+++ camp.io/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import * as L from "leaflet"
 @Component({
   selector: 'app-tab2',
@@ -8,14 +10,30 @@ import * as L from "leaflet"
 
 export class Tab2Page {
   map2: L.Map
-  inputValue = "";
+  xinputValue = 0;
+  yinputValue = 0;
   constructor() {}
 
   //https://stackoverflow.com/questions/47529327/angular-4-get-input-value
   onKey(event) {
-    this.inputValue += event.target.value;
-    // console.log(inputValue);
+    this.xinputValue = event.target.value;
+    console.log(this.xinputValue);
+    this.yinputValue = event.target.value;
+    console.log(this.yinputValue);
 
+  }
+
+  //Add new marker
+  //Took a lot of inspiration from this link https://stackoverflow.com/questions/41139546/angular2-ngsubmit-not-working
+  addLocation(form: NgForm) {
+    this.xinputValue = form.value;
+    // console.log(this.xinputValue);
+    this.yinputValue = form.value;
+    // console.log(this.yinputValue);
+    console.log(this.yinputValue);
+
+    L.marker([this.xinputValue,this.yinputValue]).addTo(this.map2)
+    .bindPopup('User Input')//36.230833,-81.676111
   }
 
   ngOnInit() {
