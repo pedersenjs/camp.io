0a35ab238eb5dae055e0548c3bee04f41e88a655
Jenna Pedersen <jspeders@ncsu.edu>
2022-10-10 21:47:49 -0400

    Added review and description functionality

--- camp.io/src/app/tab2/tab2.page.ts
+++ camp.io/src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder} from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { NgForm } from '@angular/forms';
 import * as L from "leaflet"
 @Component({
@@ -12,8 +12,10 @@ export class Tab2Page {
   map2: L.Map
   xinputValue = 0;
   yinputValue = 0;
+  rating = 0;
   locationname = "user input";
-  constructor() {}
+  description="";
+  constructor() { }
 
   //https://stackoverflow.com/questions/47529327/angular-4-get-input-value
   onKey(event) {
@@ -33,17 +35,63 @@ export class Tab2Page {
     // console.log(this.yinputValue);
     console.log(this.yinputValue);
     this.locationname = form.value.name;
+    this.rating = form.value.rating;
+    this.description=form.value.description;
     // if (this.locationname == ""){
     //   this.locationname = "user input";
     // }
+    if (this.rating == 1) {
+      L.marker([this.xinputValue, this.yinputValue]).addTo(this.map2)
+        .bindPopup(this.locationname + "<br>" +
+        "Rating: " + "<span class=\"fa fa-star checked\"></span><br>"  +
+        "Description: " + this.description)
+        
+    }
+    if (this.rating == 2) {
+      L.marker([this.xinputValue, this.yinputValue]).addTo(this.map2)
+        .bindPopup(this.locationname + "<br>" +
+        "Rating: " + "<span class=\"fa fa-star checked\"></span>" +
+        "<span class=\"fa fa-star checked \"></span><br>" +
+        "Description: " + this.description)
+        
+    }
+    if (this.rating == 3) {
+      L.marker([this.xinputValue, this.yinputValue]).addTo(this.map2)
+        .bindPopup(this.locationname + "<br>" +
+        "Rating: " + "<span class=\"fa fa-star checked\"></span>" +
+        "<span class=\"fa fa-star checked \"></span>" +
+        // <span class=\"fa fa-star checked\"></span>
+        "<span class=\"fa fa-star\"></span><br>"  +
+        "Description: " + this.description)
+        
+    }
+    if (this.rating == 4) {
+      L.marker([this.xinputValue, this.yinputValue]).addTo(this.map2)
+        .bindPopup(this.locationname + "<br>" +
+        "Rating: " + "<span class=\"fa fa-star checked\"></span>" +
+        "<span class=\"fa fa-star checked \"></span>" +
+        "<span class=\"fa fa-star checked\"></span>" +
+        "<span class=\"fa fa-star\"></span><br>"  +
+        "Description: " + this.description)
+        
+    }
+    if (this.rating == 5) {
+      L.marker([this.xinputValue, this.yinputValue]).addTo(this.map2)
+        .bindPopup(this.locationname + "<br>" +
+        "Rating: " + "<span class=\"fa fa-star checked\"></span>" +
+        "<span class=\"fa fa-star checked \"></span>" +
+        "<span class=\"fa fa-star checked\"></span>" +
+        "<span class=\"fa fa-star\"></span>"  +
+        "<span class=\"fa fa-star\"></span><br>"  +
+        "Description: " + this.description)
+        
+    }
 
-    L.marker([this.xinputValue,this.yinputValue]).addTo(this.map2)
-    .bindPopup(this.locationname)//36.230833,-81.676111
   }
 
   ngOnInit() {
     this.map2 = L.map('map2', {
-      center: [ 36.21272,-81.67292 ],
+      center: [36.21272, -81.67292],
       zoom: 13,
       renderer: L.canvas()
     })
@@ -51,8 +99,8 @@ export class Tab2Page {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     }).addTo(this.map2)
 
-    L.marker([36.21272,-81.67292]).addTo(this.map2)
-    .bindPopup('Appalachian State University')
+    L.marker([36.21272, -81.67292]).addTo(this.map2)
+      .bindPopup('Appalachian State University ')
 
     setTimeout(() => {
       this.map2.invalidateSize();
