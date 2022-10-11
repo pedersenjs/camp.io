e3850c2956137dabba8dfbd851d5bc818cd78d45
turnerpat <turnerpatrick21@gmail.com>
2022-10-11 13:41:19 -0400

    Finalized styling for week 5

--- camp.io/src/app/tab2/tab2.page.ts
+++ camp.io/src/app/tab2/tab2.page.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { NgForm } from '@angular/forms';
 import * as L from "leaflet"
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
+import { GeoSearchControl } from 'leaflet-geosearch';
+const provider = new OpenStreetMapProvider();
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -32,7 +35,7 @@ export class Tab2Page {
   //This code below is to add a new location maker
   //This link helped guide some parts, has useful tips https://stackoverflow.com/questions/41139546/angular2-ngsubmit-not-working
   addLocation(form: NgForm) {
-    //code to get input from the form and store it in variables 
+    //code to get input from the form and store it in variables
     this.xinputValue = form.value["x-coordinate"];
     this.yinputValue = form.value["y-coordinate"];
     this.locationname = form.value.name;
@@ -46,7 +49,7 @@ export class Tab2Page {
         .bindPopup(this.locationname + "<br>" +
         "Rating: " + "<span class=\"fa fa-star checked\"></span><br>"  +
         "Description: " + this.description)
-        
+
     }
     if (this.rating == 2) {
       L.marker([this.xinputValue, this.yinputValue]).addTo(this.map2)
@@ -54,7 +57,7 @@ export class Tab2Page {
         "Rating: " + "<span class=\"fa fa-star checked\"></span>" +
         "<span class=\"fa fa-star checked \"></span><br>" +
         "Description: " + this.description)
-        
+
     }
     if (this.rating == 3) {
       L.marker([this.xinputValue, this.yinputValue]).addTo(this.map2)
@@ -64,7 +67,7 @@ export class Tab2Page {
         // <span class=\"fa fa-star checked\"></span>
         "<span class=\"fa fa-star\"></span><br>"  +
         "Description: " + this.description)
-        
+
     }
     if (this.rating == 4) {
       L.marker([this.xinputValue, this.yinputValue]).addTo(this.map2)
@@ -74,7 +77,7 @@ export class Tab2Page {
         "<span class=\"fa fa-star checked\"></span>" +
         "<span class=\"fa fa-star\"></span><br>"  +
         "Description: " + this.description)
-        
+
     }
     if (this.rating == 5) {
       L.marker([this.xinputValue, this.yinputValue]).addTo(this.map2)
@@ -85,20 +88,25 @@ export class Tab2Page {
         "<span class=\"fa fa-star\"></span>"  +
         "<span class=\"fa fa-star\"></span><br>"  +
         "Description: " + this.description)
-        
+
     }
 
   }
 
   //code that is executed when the website is initialized
   ngOnInit() {
-    
+
     //create the map object using the leaflet syntax
     this.map2 = L.map('map2', {
       center: [36.21272, -81.67292],
       zoom: 13,
       renderer: L.canvas()
     })
+
+    this.map2.addControl(GeoSearchControl({
+      provider,
+      style: 'bar',
+    }))
 
     //adds the first (and currently only) map layer to the map
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
